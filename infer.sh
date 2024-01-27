3a05bdb7b4a837488ed46459e867d7d66a2e0014
Mark Peng <markpeng@exosite.com>
2024-01-27 13:20:18 +0800

    Added batch size.

--- infer.sh
+++ infer.sh
@@ -3,11 +3,13 @@
 CUDA_VISIBLE_DEVICES=0 python /kaggle/input/u-mamba/inference.py \
     --model-folder $1 \
     --output-path model_1_pred.pkl \
+    --batch-size $3 \
     --device 0 \
     --checkpoint best &
 CUDA_VISIBLE_DEVICES=1 python /kaggle/input/u-mamba/inference.py \
     --model-folder $2 \
     --output-path model_2_pred.pkl \
+    --batch-size $3 \
     --device 0 \
     --checkpoint best &
 wait
