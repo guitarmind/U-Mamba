646a222a8f6e3e1a7484ef607a29f2899f90c482
Mark Peng <markpeng@exosite.com>
2024-01-27 13:14:50 +0800

    Added batch size.

--- inference.py
+++ inference.py
@@ -22,69 +22,94 @@ from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
 
 from scipy.spatial.qhull import QhullError
 from scipy import spatial
-spatial.QhullError = QhullError 
+spatial.QhullError = QhullError
 
 parser = argparse.ArgumentParser("Inferencer")
 parser.add_argument("--model-folder", type=str, required=True,
                     help="path to model file")
 parser.add_argument("--output-path", type=str, required=True,
                     help="path to save output logits")
+parser.add_argument("--batch-size", type=int, default=8, help="Batch size")
 parser.add_argument("--device", type=int, default=0, help="device ID")
 # parser.add_argument("--threshold", type=float, default=0.001, help="logits threshold")
-parser.add_argument("--checkpoint", type=str, default="best", help="best or final")
+parser.add_argument("--checkpoint", type=str,
+                    default="best", help="best or final")
 
 args = parser.parse_args()
 # os.environ["CUDA_VISIBLE_DEVICES"] = str(args.device)
 
+
 def main(args):
-  model_folder = args.model_folder
-  output_path = args.output_path
-  # threshold = args.threshold
-  checkpoint_mode = args.checkpoint
+    model_folder = args.model_folder
+    batch_size = args.batch_size
+    output_path = args.output_path
+    # threshold = args.threshold
+    checkpoint_mode = args.checkpoint
 
-  test_image_paths = glob.glob("/kaggle/input/blood-vessel-segmentation/test/*/images/*.tif")
-  print(len(test_image_paths))
+    test_image_paths = glob.glob(
+        "/kaggle/input/blood-vessel-segmentation/test/*/images/*.tif")
+    print("test_image_paths:", len(test_image_paths))
 
-  test_dataset = []
-  test_filename = []
-  pred_image_paths = []
-  for p in test_image_paths:
-      tokens = p.split("/")
-      test_dataset.append(tokens[-3])
-      test_filename.append(tokens[-1].replace(".tif", ""))
-      pred_image_paths.append([p])
+    test_dataset = []
+    test_filename = []
+    pred_image_paths = []
+    for p in test_image_paths:
+        tokens = p.split("/")
+        test_dataset.append(tokens[-3])
+        test_filename.append(tokens[-1].replace(".tif", ""))
+        pred_image_paths.append([p])
 
-  # instantiate the nnUNetPredictor
-  predictor = nnUNetPredictor(
-      tile_step_size=0.5,
-      use_gaussian=True,
-      use_mirroring=True,
-      perform_everything_on_device=True,
-      device=torch.device('cuda', int(args.device)),
-      verbose=False,
-      verbose_preprocessing=False,
-      allow_tqdm=True
-  )
-  # initializes the network architecture, loads the checkpoint
-  predictor.initialize_from_trained_model_folder(
-      model_folder,
-      # use_folds=(0,1,2,3,4),
-      use_folds=(0,),
-      # use_folds="all",
-      checkpoint_name='checkpoint_best.pth' if checkpoint_mode == "best" else 'checkpoint_final.pth',
-  )
+    # instantiate the nnUNetPredictor
+    predictor = nnUNetPredictor(
+        tile_step_size=0.5,
+        use_gaussian=True,
+        use_mirroring=True,
+        perform_everything_on_device=True,
+        device=torch.device('cuda', int(args.device)),
+        verbose=False,
+        verbose_preprocessing=False,
+        allow_tqdm=True
+    )
+    # initializes the network architecture, loads the checkpoint
+    predictor.initialize_from_trained_model_folder(
+        model_folder,
+        # use_folds=(0,1,2,3,4),
+        use_folds=(0,),
+        # use_folds="all",
+        checkpoint_name='checkpoint_best.pth' if checkpoint_mode == "best" else 'checkpoint_final.pth',
+    )
 
-  # variant 2.5, returns segmentations
-  predicted_segmentations = predictor.predict_from_files(pred_image_paths, None,
-                                   save_probabilities=True, overwrite=True,
-                                   num_processes_preprocessing=2,
-                                   num_processes_segmentation_export=2,
-                                   folder_with_segs_from_prev_stage=None,
-                                   num_parts=1, part_id=0)
-  print("Inference finished:", len(predicted_segmentations))
-  with open(output_path, "wb") as f:
-      pickle.dump(predicted_segmentations, f, protocol=pickle.HIGHEST_PROTOCOL)
-  
+    # variant 2.5, returns segmentations
+    # predicted_segmentations = predictor.predict_from_files(pred_image_paths, None,
+    #                                                        save_probabilities=True, overwrite=True,
+    #                                                        num_processes_preprocessing=2,
+    #                                                        num_processes_segmentation_export=2,
+    #                                                        folder_with_segs_from_prev_stage=None,
+    #                                                        num_parts=1, part_id=0)
+
+    # variant 2.5, returns segmentations
+    predicted_segmentations = []
+    for i in range(0, len(pred_image_paths), batch_size):
+        start_index = i
+        end_index = i + batch_size
+
+        if end_index > len(pred_image_paths):
+            end_index = len(pred_image_paths)
+
+        preds = predictor.predict_from_files(pred_image_paths[start_index:end_index], None,
+                                             save_probabilities=True, overwrite=True,
+                                             num_processes_preprocessing=2,
+                                             num_processes_segmentation_export=2,
+                                             folder_with_segs_from_prev_stage=None,
+                                             num_parts=1, part_id=0)
+
+        predicted_segmentations.extend(preds)
+
+    print("Inference finished:", len(predicted_segmentations))
+    with open(output_path, "wb") as f:
+        pickle.dump(predicted_segmentations, f,
+                    protocol=pickle.HIGHEST_PROTOCOL)
+
 
 if __name__ == '__main__':
     main(args)
